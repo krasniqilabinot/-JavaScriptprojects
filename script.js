b297f43effaa0ed8ac8e956b10f657ad8bde3596
krasniqilabinot <labkrasniqi09@gmail.com>
2023-03-28 21:03:15 +0200

    update

--- script.js
+++ script.js
@@ -1,77 +1,135 @@
-let nasaItems = []
-const divbody=document.getElementById('near_earth_objects')
-const details = document.getElementById("details")
+let nasaItems = [];
+const defaultImage =
+  "https://www.catipilla.com/wp-content/uploads/2023/02/test-clip-art-cpa-school-test.png";
+const detailsModal = document.getElementById("detailsModal");
+const details = document.getElementById("details");
+const list = document.getElementById("myList");
+const closeIcon = document.getElementById("closeIcon");
 
-let pageCount = 1
+let pageNumber = 1;
+const pageSize = 10;
 
-
-function clearData() {
-    details.innerHTML=""
+function clearData(element) {
+  element.innerHTML = "";
 }
 
+function closeOpenModal() {
+  detailsModal.style.display =
+    detailsModal.style.display === "block" ? "none" : "block";
+}
 
-
-async function getData() {
-    let list = document.getElementById("myList");
-    list.innerHTML = ""
-    clearData()
-    const year_start = document.getElementById("start_year")
-    const year_end = document.getElementById("end_year")
-    const res = await axios.get(`https://images-api.nasa.gov/search?year_start=${year_start.value}&year_end=${year_end.value}`)
-
-   
-    nasaItems = res.data.collection.items
-    nasaItems = nasaItems.slice(0, 30)
-    nasaItems.forEach((item) => {
-      let li = document.createElement("li");
-      let img = document.createElement("img")
-      img.src = item.links ? item.links[0].href : "https://www.catipilla.com/wp-content/uploads/2023/02/test-clip-art-cpa-school-test.png"
-      li.appendChild(img)
-      let liSpan = document.createElement("span")
+function showList() {
+  clearData(list);
+  nasaItems
+    .slice((pageNumber - 1) * pageSize, pageNumber * pageSize)
+    .forEach((item) => {
+      const li = document.createElement("li");
+      const img = document.createElement("img");
+      img.src = item.links ? item.links[0].href : defaultImage;
+      li.appendChild(img);
+      const liSpan = document.createElement("span");
       liSpan.innerHTML = item.data[0].title;
-      li.appendChild(liSpan)
-      li.setAttribute('nasa-id', item.data[0].nasa_id)
+      li.appendChild(liSpan);
+      li.setAttribute("nasa-id", item.data[0].nasa_id);
       list.appendChild(li);
     });
-
 }
 
-let searchButton = document.getElementById("searchBtn")
+function validInputs(yearStart, yearEnd) {
+  if (isNaN(yearStart) || isNaN(yearEnd)) {
+    alert('Vlerat duhet te jene te tipit numer')
+    return false
+  }
+  if (yearStart.length !== 4 || yearEnd.length !== 4) {
+    alert('Vlerat duhet te kene gjatsine 4')
+    return false
+  }
 
-searchButton.addEventListener('click',(event) => {
-    getData()
-})
+  return true
+}
 
-myList.addEventListener("click",(event)=>{
-    // console.log(event.target.getAttribute('nasa-id'))
-    const selectedNasaId = event.target.getAttribute('nasa-id')
-    const itemFound = nasaItems.find((item) => {
-        return item.data[0].nasa_id === selectedNasaId 
-    })
-    if(itemFound) {
-       
-        clearData()
-        let closeIcon=document.createElement("div")
-        closeIcon.innerHTML="X"
-        closeIcon.setAttribute("class","closeIcon")
-        details.appendChild(closeIcon)
-        let title = document.createElement("h3")
-        title.innerHTML = itemFound.data[0].title
-        details.appendChild(title)
-        let img = document.createElement("img")
-        img.src = itemFound.links ? itemFound.links[0].href : "https://www.catipilla.com/wp-content/uploads/2023/02/test-clip-art-cpa-school-test.png"
-        details.appendChild(img)
-        let description = document.createElement("div")
-        description.innerHTML = itemFound.data[0].description
-        details.appendChild(description)
+async function getData() {
+  const year_start = document.getElementById("start_year");
+  const year_end = document.getElementById("end_year");
+  if (validInputs(year_start.value, year_end.value)) {
+    clearData(list);
+    clearData(details);
+    const res = await axios.get(
+      `https://images-api.nasa.gov/search?year_start=${year_start.value}&year_end=${year_end.value}`
+    );
 
-        closeIcon.addEventListener("click",()=>{
-            clearData()
-        })
-    }
-    console.log({itemFound})
-})
+    nasaItems = res.data.collection.items;
+    showList();
+  }
+}
 
+function addTitleToDetails(itemTitle) {
+  const title = document.createElement("h3");
+  title.innerHTML = itemTitle;
+  details.appendChild(title);
+}
 
-getData()
+function addImageToDetails(url) {
+  let img = document.createElement("img");
+  img.src = url;
+  details.appendChild(img);
+}
 
+function addDescriptionToImage(itemDescription) {
+  let description = document.createElement("div");
+  description.innerHTML = itemDescription;
+  details.appendChild(description);
+}
+
+closeIcon.addEventListener("click", () => {
+  closeOpenModal();
+  clearData(details);
+});
+
+myList.addEventListener("click", (event) => {
+  const selectedNasaId = event.target.getAttribute("nasa-id");
+  const itemFound = nasaItems.find((item) => {
+    return item.data[0].nasa_id === selectedNasaId;
+  });
+
+  if (itemFound) {
+    closeOpenModal();
+    clearData(details);
+    addTitleToDetails(itemFound.data[0].title);
+    addImageToDetails(itemFound.links ? itemFound.links[0].href : defaultImage);
+    addDescriptionToImage(itemFound.data[0].description);
+  }
+});
+
+// pagination
+const btnPrev = document.getElementById("btn_prev");
+const btnNext = document.getElementById("btn_next");
+
+btnNext.addEventListener("click", () => {
+  if (pageSize * pageNumber < nasaItems.length) {
+    pageNumber += 1;
+    showList();
+  } else {
+    alert("Ju keni arritur ne fund te listes");
+  }
+});
+
+btnPrev.addEventListener("click", (event) => {
+  if (pageNumber > 1) {
+    pageNumber -= 1;
+    showList();
+  } else {
+    alert("Ju keni arritur ne fillim te listes");
+  }
+});
+
+// end pagination
+
+const searchButton = document.getElementById("searchBtn");
+
+searchButton.addEventListener("click", () => {
+  pageNumber = 1
+  getData();
+});
+
+getData();
